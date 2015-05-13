d9d5e78f0408586bb4e87f83d7443affc8ead8ee
Ezra Keddell <ezrakeddell@gmail.com>
2015-05-13 16:33:00 +1200

    first commit

---
+++ dragdealer/js/drag.js
@@ -0,0 +1,48 @@
+$(function() {
+  var s = new DonutSlider('donut-slider', 30, '#aa0000', '#444');
+  s.setValue(.5);
+});
+
+////////////////////////
+
+var DonutSlider = function(elementId, borderSize, color1, color2) {
+  var _this = this,
+    size = Math.min($('#' + elementId).width(),
+      $('#' + elementId).height());
+  this.pie = d3.layout.pie().sort(null);
+  this.arc = d3.svg.arc()
+    .innerRadius((size / 2) - borderSize)
+    .outerRadius(size / 2);
+  this.svg = d3.select('#' + elementId).append('svg')
+    .attr('width', size)
+    .attr('height', size)
+    .append('g')
+    .attr('transform', 'translate(' + size / 2 + ',' + size / 2 + ')');
+  this.colors = [color1, color2];
+  this.path = this.svg.selectAll('path')
+    .data(this.pie([0, 100]))
+    .enter().append('path')
+    .attr('fill', function(d, i) {
+      return _this.colors[i];
+    })
+    .attr('d', this.arc);
+  this.dragdealer = new Dragdealer(elementId, {
+    x: 1,
+    slide: false,
+    animationCallback: function(x, y) {
+      _this.updateChart((1 - x) * 100);
+      $('#' + elementId).find('.value').text(Math.round((1 - x) * 100));
+    }
+  });
+};
+DonutSlider.prototype = {
+  setValue: function(value) {
+    this.dragdealer.setValue(1 - value, 0, true);
+  },
+  updateChart: function(value) {
+    this.path.data(this.pie([value, 100 - value]))
+      .attr('d', this.arc);
+  }
+};
+
+// Forked from http://codepen.io/skidding/pen/bKvoA/
